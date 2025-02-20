039b2f0af5566e7d042aa24412026e651d8f6c8e
Dov Goldenthal <dgoldenthal@gmail.com>
2025-02-21 10:34:08 +1300

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,24 @@
 import { useContext } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import ApplyJob from './pages/ApplyJob'
-import Applications from './pages/Applications'
-import RecruiterLogin from './components/RecruiterLogin'
-import { AppContext } from './context/AppContext'
-import Dashboard from './pages/Dashboard'
-import AddJob from './pages/AddJob'
-import ManageJobs from './pages/ManageJobs'
-import ViewApplications from './pages/ViewApplications'
-import Login from './pages/Login'  // Add this import
+import { AppContext } from '@/context/AppContext'
+import { ToastContainer } from 'react-toastify'
+
+// Components
+import RecruiterLogin from '@/components/RecruiterLogin'
+
+// Pages
+import Home from '@/pages/Home'
+import Login from '@/pages/Login'
+import ApplyJob from '@/pages/ApplyJob'
+import Applications from '@/pages/Applications'
+import Dashboard from '@/pages/Dashboard'
+import AddJob from '@/pages/AddJob'
+import ManageJobs from '@/pages/ManageJobs'
+import ViewApplications from '@/pages/ViewApplications'
+
+// Styles
 import 'quill/dist/quill.snow.css'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
   const { showRecruiterLogin, companyToken } = useContext(AppContext)
@@ -23,17 +29,17 @@ const App = () => {
       <ToastContainer />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />  {/* Add this route */}
+        <Route path='/login' element={<Login />} />
         <Route path='/apply-job/:id' element={<ApplyJob />} />
         <Route path='/applications' element={<Applications />} />
         <Route path='/dashboard' element={<Dashboard />}>
-          {
-            companyToken ? <>
+          {companyToken && (
+            <>
               <Route path='add-job' element={<AddJob />} />
               <Route path='manage-jobs' element={<ManageJobs />} />
               <Route path='view-applications' element={<ViewApplications />} />
-            </> : null
-          }
+            </>
+          )}
         </Route>
       </Routes>
     </div>
--- client/vite.config.js
+++ client/vite.config.js
@@ -6,28 +6,10 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-    extensions: ['.js', '.jsx', '.json']
+      '@': path.resolve(__dirname, './src')
+    }
   },
   build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    sourcemap: true,
-    chunkSizeWarningLimit: 1500,
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'),
-      output: {
-        manualChunks: {
-          vendor: [
-            'react',
-            'react-dom',
-            'react-router-dom',
-            'react-quill',
-            'axios'
-          ]
-        }
-      }
-    }
+    outDir: 'dist'
   }
 })
