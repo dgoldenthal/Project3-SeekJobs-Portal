a8f6c22fac7f33b08ee035cd00d1099e0429c655
Dov Goldenthal <dgoldenthal@gmail.com>
2025-02-21 00:54:01 +1300

    updates

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,23 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   server: {
     port: 5173,
-    open: true,
     proxy: {
-      '/api': 'http://localhost:5000',
-    },
+      '/api': 'http://localhost:5000'
+    }
   },
-  plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    chunkSizeWarningLimit: 1600,
-  },
-});
+    emptyOutDir: true,
+    rollupOptions: {
+      external: ['@rollup/rollup-linux-x64-gnu']
+    }
+  }
+})
