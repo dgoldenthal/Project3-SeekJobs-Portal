183a9cfb9187f1cc026eab672e6d767bd53b5470
Dov Goldenthal <dgoldenthal@gmail.com>
2025-02-21 11:07:48 +1300

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+    extensions: ['.mjs', '.js', '.jsx', '.json']
+  },
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            if (id.includes('react-toastify') || 
+                id.includes('axios') || 
+                id.includes('quill')) {
+              return 'vendor'
+            }
+          }
+        }
+      }
+    }
   }
 })
