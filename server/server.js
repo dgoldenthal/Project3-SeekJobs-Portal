f2310ecd23dd0ff8729e87d6156a13b8e5ce2e1d
Dov Goldenthal <dgoldenthal@gmail.com>
2025-02-21 00:12:48 +1300

    update files

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import connectDB from './config/db.js';
 import connectCloudinary from './config/cloudinary.js';
 import companyRoutes from './routes/companyRoutes.js';
@@ -10,24 +12,36 @@ import userRoutes from './routes/userRoutes.js';
 // Initialize Express
 const app = express();
 
+// ES Module fix for __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // Connect to database
 const initializeServer = async () => {
     await connectDB();
     await connectCloudinary();
 };
 
+// Initialize server
+initializeServer();
+
 // Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Serves static files in the entire client's dist folder
-app.use(express.static('../client/dist'));
-
 // Routes
-app.get('/', (req, res) => res.send("API Working"));
 app.use('/api/company', companyRoutes);
 app.use('/api/jobs', jobRoutes);
 app.use('/api/users', userRoutes);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
 // Port
 const PORT = process.env.PORT || 5000;
